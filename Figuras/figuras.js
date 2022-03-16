9110b04acf2452756271f8486befa3c6e3b1afec
diegoderivera <djoromac@hotmail.com>
2022-03-16 01:21:03 -0600

    Si me pasé no había hecho ningún commit, se agregó absolutamente todo el HTML y el CSS, se empezó a trabajar el JS

--- Figuras/figuras.js
+++ Figuras/figuras.js
@@ -1,19 +1,37 @@
+let x;
+const PI = Math.PI;
+
+// Agregando los botones a JS y sus Event Listeners, además del <p> para mostrar el resultado
+let sp = document.getElementById("square-perimeter");
+sp.addEventListener("click", squarePerimeter);
+
+let sa = document.getElementById("square-area");
+sa.addEventListener("click", squareArea);
+
+const sr = document.getElementById("square-result");
+
+let tp = document.getElementById("triangle-perimeter");
+tp.addEventListener("click", trianglePerimeter);
+
+let ta = document.getElementById("triangle-area");
+ta.addEventListener("click", triangleArea);
+
+const tr = document.getElementById("triangle-result");
+
+let cp = document.getElementById("circle-perimeter");
+cp.addEventListener("click", circlePerimeter);
+
+let ca = document.getElementById("circle-area");
+ca.addEventListener("click", circleArea);
+
+const cr = document.getElementById("circle-result");
 //Código del Cuadrado
 
-console.group("Cuadrado");
+squarePerimeter(){
+    x = getElementById("square-input");
+    const result = x * 4;
+}
 
-let ladoCuadrado = 5;
-console.log("Los lados del cuadrado son: " + ladoCuadrado + "cm");
-
-const perimetroCuadrado = (lado) => lado * 4
-
-console.log("El perímetro del cuadrado es: " + perimetroCuadrado(ladoCuadrado) + "cm");
-
-const areaCuadrado = (lado) => lado ** 2;
-
-console.log("El área del cuadrado es: " + areaCuadrado(ladoCuadrado) + "cm^2");
-
-console.groupEnd(); 
 //Código del Triángulo
 
 console.group("Triángulo");
@@ -59,7 +77,6 @@ let diametroCirculo = radioCirculo * 2;
 
 console.log("El diámetro del círculo es: " + diametroCirculo + " cm");
 
-const PI = Math.PI;
 
 console.log("Pi es: " + PI);
 
