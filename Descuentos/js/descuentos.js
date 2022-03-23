33b253ca6f154c680a86715c6ad0f6fb1d1100a5
diegoderivera <djoromac@hotmail.com>
2022-03-22 23:05:56 -0600

    Se agregó la estructura HTML del segundo taller, y se comenzó con el JS

---
+++ Descuentos/js/descuentos.js
@@ -0,0 +1,16 @@
+const price = document.getElementById("itemPrice");
+const discount = document.getElementById("itemDiscount");
+const coupon = document.getElementById("itemCoupon");
+let x;
+let y;
+let z;
+
+
+function discountOperation(){
+    x = price.value;
+    y = discount.value;
+    z = coupon.value;
+
+
+
+}
--- README.md
+++ README.md
@@ -10,3 +10,8 @@
 - Cuarto paso: Integrar javascript con HTML
 
 ## Taller 2: Porcentajes y Descuentos
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JS
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar javacript con HTML
